617f837aee1434d00a5eec3e1340d47c9ec85026
flauta <hector.nieto@utec.edu.pe>
2025-09-20 12:18:00 -0500

    unpack serializer for generic data

--- indexes/extendible_hashing/extendible_hashing.py
+++ indexes/extendible_hashing/extendible_hashing.py
@@ -11,7 +11,8 @@ class Serializer:
     def pack(self, record):
         pack = []
         for (name, format) in self.record_format:
-            val = getattr(record, name) #get val of attr
+            val = getattr(record, name) #we get from the SimpleNameSpace object the value of the attribute specified
+                #in the record-format given by the parser
             if format[0] == '*':
                 n = len(val)
                  #doesnt support array of strings
@@ -20,8 +21,28 @@ class Serializer:
             else:
                 if 's' in format: #20s eg.
                     val = val.encode()
-                #we get from the SimpleNameSpace object the value of the attribute specified
-                #in the record-format given by the parser
                 pack.append(struct.pack(format, val))
 
         return b''.join(pack)
+
+    @staticmethod
+    def unpack(self, data):
+        record = SimpleNamespace()
+        for (name, format) in self.record_format: #according to the format
+            if format[0] == '*': #if its varialbe length
+                n = struct.unpack('i', data[:4])[0] #we get the amount, [0] because it returns a tuple
+                val = struct.unpack(f'{n}' + format[1:], data[4:4+n*struct.calcsize(format[1:])]) #multiple values, dont use [0]
+                #then we unpack the quantity after the int and until the total elements using calcsize
+                val = list(val)
+                data = data[4+n*struct.calcsize(format[1:]):] #slice each time we do this so we dont use an offset
+            else:
+                val = struct.unpack(format, data[:struct.calcsize(format)])[0]
+                if 's' in format:
+                    val = val.decode().strip('\x00') #decode if string
+                #we unpack according to format
+                # till the size of the format
+                data = data[struct.calcsize(format):] #slice data
+            setattr(record, name, val) #we set the attributes
+        return record
+
+
