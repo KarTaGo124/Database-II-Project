87bd75f9d66cb131800cc83d7f2afdbf7ed7f6ea
flauta <hector.nieto@utec.edu.pe>
2025-09-23 22:01:07 -0500

    change from storing record to storing index, add logic to get dir index

--- indexes/extendible_hashing/extendible_hashing.py
+++ indexes/extendible_hashing/extendible_hashing.py
@@ -2,8 +2,8 @@
 import struct
 from ..core.record import Record
 import os
+
 BLOCK_FACTOR = 4
-RECORD_SIZE = 100
 
 
 # max overflow 1 bucket
@@ -16,19 +16,24 @@ class Bucket:
         self.size = size
         self.next_bucket = next_bucket
 
-    def insert(self, data: bytes, file, bucket_pos: int):
+    def insert(self, record: Record, datafile, indexfile, bucket_pos: int):
         # 1) check if size < block factor
         if self.size >= BLOCK_FACTOR:
             return False  # split or overflow
-        # 2) write record
-        file.seek(bucket_pos + self.HEADER_SIZE + self.size * RECORD_SIZE)
-        file.write(data)
 
+        # 2) write record to data file at the end
+        datafile.seek(0, 2)
+        offset = datafile.tell()
+        datafile.write(record.pack())  # assume Record.pack() returns fixed 100B
+
+        # 3) store offset in bucket
+        indexfile.seek(bucket_pos + self.HEADER_SIZE + self.size * 4)
+        indexfile.write(struct.pack("i", offset))
+
+        # 4) update header
         self.size += 1
-
-        file.seek(bucket_pos)
-
-        file.write(struct.pack(
+        indexfile.seek(bucket_pos)
+        indexfile.write(struct.pack(
             self.HEADER_FORMAT,
             self.local_depth,
             self.size,
@@ -37,31 +42,33 @@ class Bucket:
 
         return True
 
-
-    def search(self, file, bucket_pos: int, key: bytes):
-        if self.size == 0:
-            return None
-        file.seek(bucket_pos)
+    @staticmethod
+    def search(datafile, indexfile, bucket_pos: int, key: bytes):
+        # read header
+        indexfile.seek(bucket_pos)
         local_depth, size, next_bucket = struct.unpack(
-            self.HEADER_FORMAT, file.read(self.HEADER_SIZE)
+            Bucket.HEADER_FORMAT, indexfile.read(Bucket.HEADER_SIZE)
         )
 
+        # scan pointers
         for i in range(size):
-            file.seek(bucket_pos + self.HEADER_SIZE + i * RECORD_SIZE)
-            record = file.read(RECORD_SIZE)
-            record = Record.unpack(record)
+            indexfile.seek(bucket_pos + Bucket.HEADER_SIZE + i * 4)
+            offset = struct.unpack("i", indexfile.read(4))[0]
+
+            # go to data file and check record
+            datafile.seek(offset)
+            record = Record.unpack(datafile.read(Record.RECORD_SIZE))
             if record.key == key:
                 return record
 
-        #if not found
-        # check overflow bucket if any
+        # check overflow bucket
         if next_bucket != -1:
-            return self.search(file, next_bucket, key)
+            return Bucket.search(datafile, indexfile, next_bucket, key)
 
         return None
 
 
-class ExtendableHashing: #pointers
+class ExtendableHashing:  # directory
     HEADER_FORMAT = "ii"  # globaldepth, num of elements
     HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
     DIR_FORMAT = "i"  # pointer to bucket
@@ -70,14 +77,14 @@ class ExtendableHashing: #pointers
     def __init__(self, filename, indexname, global_depth=3, size=0):
         self.filename = filename
         self.indexname = indexname
-        self.file = open(filename, 'r+b')
+        self.file = open(filename, 'r+b')   # data file
         create_new = not os.path.exists(indexname)
-        self.indexfile = open(indexname, 'w+b' if create_new else 'r+b')
+        self.indexfile = open(indexname, 'w+b' if create_new else 'r+b')  # index file
         self.size = size
         self.global_depth = global_depth
-        # create initial 2 buckets and have directories ending in 0 or 1 point to the respective 1,
-        # write this in the indexfile
+
         if create_new:
+            # write header
             self.indexfile.seek(0)
             self.indexfile.write(struct.pack(
                 self.HEADER_FORMAT,
@@ -88,39 +95,42 @@ class ExtendableHashing: #pointers
             dir_entries = 2 ** self.global_depth
             dir_start = self.HEADER_SIZE
 
-            # 2) create two initial buckets
-            bucket0 = Bucket(local_depth=1, size=0, next_bucket=-1)
-            bucket1 = Bucket(local_depth=1, size=0, next_bucket=-1)
+            # make 2 initial buckets
+            bucket0 = Bucket(local_depth=1)
+            bucket1 = Bucket(local_depth=1)
 
-            # space for each bucket (header + record slots)
-            bucket_space = Bucket.HEADER_SIZE + BLOCK_FACTOR * RECORD_SIZE
+            bucket_space = Bucket.HEADER_SIZE + BLOCK_FACTOR * 4
 
             bucket0_pos = dir_start + dir_entries * self.DIR_SIZE
             bucket1_pos = bucket0_pos + bucket_space
 
-            # write empty bucket0
+            # write empty headers
             self.indexfile.seek(bucket0_pos)
             self.indexfile.write(struct.pack(
-                Bucket.HEADER_FORMAT,
-                bucket0.local_depth,
-                bucket0.size,
-                bucket0.next_bucket
+                Bucket.HEADER_FORMAT, bucket0.local_depth, 0, -1
             ))
-
-            # write empty bucket1
             self.indexfile.seek(bucket1_pos)
             self.indexfile.write(struct.pack(
-                Bucket.HEADER_FORMAT,
-                bucket1.local_depth,
-                bucket1.size,
-                bucket1.next_bucket
+                Bucket.HEADER_FORMAT, bucket1.local_depth, 0, -1
             ))
 
             # init directory
             self.indexfile.seek(dir_start)
-
             for i in range(dir_entries):
-                if i % 2 == 0:  # even last bit 0
+                if i % 2 == 0:
                     self.indexfile.write(struct.pack(self.DIR_FORMAT, bucket0_pos))
-                else:  # odd last bit
-                    self.indexfile.write(struct.pack(self.DIR_FORMAT,bucket1_pos))
+                else:
+                    self.indexfile.write(struct.pack(self.DIR_FORMAT, bucket1_pos))
+
+    def get_dir_index(self, key):
+        hash_val = hash(key)
+        return hash_val % (2 ** self.global_depth)
+
+    def search(self, key):
+        if self.size == 0:
+            return None
+        dir_index = self.get_dir_index(key)
+        self.indexfile.seek(self.HEADER_SIZE + dir_index * self.DIR_SIZE)
+        bucket_pos = struct.unpack(self.DIR_FORMAT, self.indexfile.read(self.DIR_SIZE))[0]
+
+        return Bucket.search(self.file, self.indexfile, bucket_pos, key)
