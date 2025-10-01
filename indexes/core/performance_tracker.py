95ce72cb96fc3f7e63a5778a01aa70111f0c3126
KarTaGo124 <joseguillermogalvezpacori@gmail.com>
2025-10-01 16:15:01 -0500

    add: flag rebuild triggered

--- indexes/core/performance_tracker.py
+++ indexes/core/performance_tracker.py
@@ -1,15 +1,17 @@
 import time
 
 class OperationResult:
-    def __init__(self, data, execution_time_ms, disk_reads, disk_writes):
+    def __init__(self, data, execution_time_ms, disk_reads, disk_writes, rebuild_triggered=False):
         self.data = data
         self.execution_time_ms = execution_time_ms
         self.disk_reads = disk_reads
         self.disk_writes = disk_writes
         self.total_disk_accesses = disk_reads + disk_writes
+        self.rebuild_triggered = rebuild_triggered
 
     def __repr__(self):
-        return f"OperationResult(data={self.data}, time={self.execution_time_ms:.2f}ms, accesses={self.total_disk_accesses})"
+        rebuild_info = " [REBUILD]" if self.rebuild_triggered else ""
+        return f"OperationResult(data={self.data}, time={self.execution_time_ms:.2f}ms, accesses={self.total_disk_accesses}{rebuild_info})"
 
 class PerformanceTracker:
     def __init__(self):
@@ -31,6 +33,6 @@ class PerformanceTracker:
     def track_write(self):
         self.writes += 1
 
-    def end_operation(self, result_data):
+    def end_operation(self, result_data, rebuild_triggered=False):
         execution_time = (time.time() - self.start_time) * 1000
-        return OperationResult(result_data, execution_time, self.reads, self.writes)
+        return OperationResult(result_data, execution_time, self.reads, self.writes, rebuild_triggered)
