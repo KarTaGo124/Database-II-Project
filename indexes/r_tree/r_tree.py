41695feae061c32a3accf075855734e890a605b7
matiasmenesess <matias.meneses@utec.edu.pe>
2025-10-01 11:57:46 -0500

    prime, listo el knnnnnnnnn no usa lo que vimos en clase exactamente, pq la libreria en si trabaja de forma diferente y ya me da metodos que me simplifican la vida

--- indexes/r_tree/r_tree.py
+++ indexes/r_tree/r_tree.py
@@ -52,7 +52,24 @@ class RTree:
 
     #def rangeSearch ( con el point, radio)
 
-    #def rangeSearch (con el point, k)
+    #def rangeSearch (con el point, k) le voy a poner KNN pq no se como diferenciar si es con radio o con k
+    def KNN(self, punto: List[float], k: int) -> List[dict]:
+        if k<=0:
+            raise ValueError("K DEBE SER MAYOR A 0")
+        caja = self.punto_a_rectangulo(punto)
+        id_k_vecinos = list(self.idx.nearest(caja, k))
+        k_vecinos = []
+        for id_record in id_k_vecinos:
+            record = self.records.get(id_record)
+            if record:
+                dist = self.distancia_euclidiana(tuple(punto), tuple(record['punto']))
+                resultado = record.copy()
+                resultado['distancia'] = dist
+                k_vecinos.append(resultado)
+
+        k_vecinos.sort(key=lambda x: x['distancia'])
+        return k_vecinos[:k]
+
 
     #def rangeSearch (begin-key, end-key) va?????
 
@@ -67,10 +84,23 @@ class RTree:
         except Exception as e:
             print(f"ERROR AL AÑADIR: {e}")
             return False
-        
 
-    def remove():
-        pass
+    def remove(self, id_record: int) -> bool:
+        record = self.records.get(id_record)
+        if not record:
+            print("ERROR: EL ID NO EXISTE")
+            return False
+        try:
+            caja = self.punto_a_rectangulo(record['punto'])
+            self.idx.delete(id_record, caja)
+            del self.records[id_record]
+
+            if self.metadata_file:
+                self.save_metadata()
+            return True
+        except Exception as e:
+            print(f"ERROR AL ELIMINAR: {e}")
+            return False
 
     def distancia_euclidiana(self, punto1: Tuple[float], punto2: Tuple[float]) -> float:
         if len(punto1) != len(punto2):
@@ -81,14 +111,10 @@ class RTree:
         return math.sqrt(suma)
 
     def punto_a_rectangulo(self, punto: List[float]) -> Tuple:
-        # el rtree piensa con cajas, no con puntos por ejemplo el punto (3,4) en realidad es la caja (3,4,3,4) y asi
+        # el rtree (la libreria) piensa con cajas, no con puntos por ejemplo el punto (3,4) en realidad es la caja (3,4,3,4) y asi
         if len(punto) != self.dimension:
             raise ValueError(f"El punto debe tener {self.dimension} dimensiones")
         return tuple(punto + punto)
-
-
-
-        return tuple(min_c + max_c)
 
     def close(self):
         if self.metadata_file:
