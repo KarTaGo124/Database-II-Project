5832e5cecdc0a4604dc6fbda1cece4418e94bede
matiasmenesess <matias.meneses@utec.edu.pe>
2025-09-30 21:02:25 -0500

    iniciando el rtree lo basico aun

--- indexes/r_tree/r_tree.py
+++ indexes/r_tree/r_tree.py
@@ -1,1 +1,32 @@
 # R Tree Index
+import math
+import pickle
+import os
+from rtree import index
+from typing import List, Tuple, Any, Optional
+
+
+
+class RTree:
+    def __init__(self, dimension: int = 2, filename: str = None):
+    #pq no pongo? m ni M, esto se debe a que la libreria de rtree ya usa valores por defecto que estan optimizados para la mayoria de los casos
+        self.dimension = dimension
+        self.filename = filename
+
+        p = index.Property() #estocrea el objeto 
+        p.dimension = dimension 
+        if filename:
+            self.idx = index.Index(filename, properties=p)
+            self.metadata_file = f"{filename}_metadata.pkl"
+        else:
+            self.idx = index.Index(properties=p)
+            self.metadata_file = None
+
+            self.records = {}
+        
+        if self.metadata_file and os.path.exists(self.metadata_file):
+            self._load_metadata() # basicamente carga el archivo si existe para no perder los datos
+        
+
+
+        
